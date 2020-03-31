7a14882de0a9c461b6d856a2d9af417df57eb11c
DSawner <62436010+DSawner@users.noreply.github.com>
2020-03-31 10:16:20 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource ibm_is_vpc "vpc" {
 
 resource ibm_is_security_group "sg1" {
   name = "${local.BASENAME}-sg1"
-  vpc  =  ${ibm_is_vpc.vpc.id}"
+  vpc  = "${ibm_is_vpc.vpc.id}"
 }
 
 # allow all incoming network traffic on port 22
@@ -34,7 +34,7 @@ resource "ibm_is_security_group_rule" "ingress_ssh_all" {
 
 resource ibm_is_subnet "subnet1" {
   name = "${local.BASENAME}-subnet1"
-  vpc  = ${ibm_is_vpc.vpc.id}"
+  vpc  = "${ibm_is_vpc.vpc.id}"
   zone = "${local.ZONE}"
   total_ipv4_address_count = 256
 }
@@ -76,5 +76,5 @@ output sshcommand {
 }
 
 output vpc_id {
- value =  ${ibm_is_vpc.vpc.id}"
+ value =  "${ibm_is_vpc.vpc.id}"
 }
