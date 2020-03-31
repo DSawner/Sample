c31a6985fdd78810f351a4c529c26105ce2f4c7a
DSawner <62436010+DSawner@users.noreply.github.com>
2020-03-31 10:44:40 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,4 +14,7 @@ resource "ibm_is_subnet" "subnet1" {
   zone                     = "${local.ZONE}"
   total_ipv4_address_count = 256
 }
-
+resource "ibm_is_security_group" "sg1" {
+  name = "${local.BASENAME}-sg1"
+  vpc  = "${ibm_is_vpc.vpc.id}"
+}
